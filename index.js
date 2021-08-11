5eb608af5cbdeb5cd489f5174795c0004f73cc1d
Relisource <relisource@makMashuk-2.local>
2021-08-12 00:37:19 +0600

    Updated

--- index.js
+++ index.js
@@ -3,25 +3,14 @@ var cors = require('cors')
 const api_helper = require('./helper')
 const app = express()
 const port = process.env.PORT || 3000
-const apikey  = '20f7cbb365314ec3a4104d0f1f9410ad';
+const apikey  = '668e8a61946f4307b216293b6b4b19cf';
+// const apikey  = '20f7cbb365314ec3a4104d0f1f9410ad';
 
 app.use(cors());
-app.get('/', (req, res) => {
-//   res.send('Hello World!');
-console.log("ok");
-  api_helper.make_API_call('https://newsapi.org/v2/everything?q=bitcoin&apiKey='+apikey)
-  .then(response => {
-      res.json(response)
-  })
-  .catch(error => {
-      res.send(error)
-  })
-})
-app.get('/top-headlines',(req, res)=>{
 
+app.get('*',(req, res)=>{
 
-//   res.send();
-  const url = `https://newsapi.org/v2/${req.originalUrl}&apiKey=${apikey}`;
+  const url = `https://newsapi.org/v2${req.originalUrl}&apiKey=${apikey}`;
   api_helper.make_API_call(url)
   .then(response => {
       res.json(response)
